80a2bb2509a537b9c1d6ace1a46b20c29f7c0282
dennis <murildennis7@gmail.com>
2022-01-15 16:13:47 +0300

    Added Python runtime.

--- Todo/settings.py
+++ Todo/settings.py
@@ -127,3 +127,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
