e19c653c56e957ac7d0588a5ad9966581016899e
dennis <murildennis7@gmail.com>
2022-01-15 15:10:54 +0300

    Second commit.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    task = models.CharField(max_length=200)
+    start_time = models.TimeField()
+    stop_time = models.TimeField()
+    start_date = models.DateField()
+    stop_date = models.DateField()
+    complete = models.BooleanField(null=True, blank=True)
+
+    def __str__(self):
+        return self.task
